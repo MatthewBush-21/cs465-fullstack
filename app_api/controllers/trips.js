4dc8e54d94a7df1c765c0fa3a80cf4e58e75c23c
Matthew Bush <matthewbush1111@gmail.com>
2026-06-14 16:23:25 -0400

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -4,25 +4,20 @@ const Trip = mongoose.model('trips');
 
 const tripsList = async (req, res) => {
     try {
-      
         const trips = await Trip.find({}).exec();
-
         console.log(trips);
-
-       
+        
         if (!trips || trips.length === 0) {
             return res
                 .status(404)
                 .json({ "message": "No trips found in the database" });
         } 
         
-       
         return res
             .status(200)
             .json(trips);
 
     } catch (err) {
-       
         return res
             .status(500)
             .json(err);
@@ -50,7 +45,71 @@ const tripsFindByCode = async (req, res) => {
     }
 };
 
+const tripsAddTrip = async (req, res) => {
+    try {
+        const newTrip = await Trip.create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        });
+        
+        return res
+            .status(201)
+            .json(newTrip);
+            
+    } catch (err) {
+        return res
+            .status(400) 
+            .json(err);
+    }
+};
+
+
+const tripsUpdateTrip = async (req, res) => {
+    
+    
+    try {
+        const q = await Trip.findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            },
+            { new: true } 
+        ).exec();
+        
+        if (!q) { 
+            
+            return res
+                .status(404)
+                .json({ message: "Trip not found for updating" });
+        } else { 
+            
+            return res
+                .status(200)
+                .json(q);
+        }
+    } catch (err) {
+        return res
+            .status(400)
+            .json(err);
+    }
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
